1cc4f627fc847336dffefa4eec2c46f9f3432c34
Richard Guthrie <rguthriemsft@users.noreply.github.com>
2022-06-05 18:04:14 +0000

    Add linkerd prework

---
+++ .devcontainer/scripts/postStart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Condifure and start octant
+mkdir ./.devcontainer/logs/ -p
+nohup bash -c 'octant --disable-origin-check --disable-open-browser &' > ./.devcontainer/logs/octant.log
+
+# Configure path for linkerd
+echo 'export PATH=$PATH:/home/vscode/.linkerd2/bin' >> ~/.bashrc
